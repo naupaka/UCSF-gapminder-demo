cbd82691276c0b509c9db48c5f92f24a567c8e39
Naupaka Zimmerman <naupaka@gmail.com>
2017-03-06 11:27:10 -0800

    Delete comment line

--- 01_setupProject.sh
+++ 01_setupProject.sh
@@ -5,8 +5,6 @@
 # unzip it, 
 ##########################################################
 
-# This is a comment added on GitHub
-
 echo Creating project directory structure
 sleep 2
 mkdir -v data code output data/archive
